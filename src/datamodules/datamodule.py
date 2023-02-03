5e601d49a2a0dde232c7f7aca0ad032f159db35e
phqlong <phqlong2810@gmail.com>
2023-02-03 15:00:24 +0700

    Add Meta Datamodule for Meta Learning Dataset

--- src/datamodules/datamodule.py
+++ src/datamodules/datamodule.py
@@ -1,45 +1,30 @@
+import hydra
+import omegaconf
+from omegaconf import DictConfig
+
+import random
 from typing import Any, Dict, Optional, Tuple
 
 import torch
 from pytorch_lightning import LightningDataModule
-from torch.utils.data import ConcatDataset, DataLoader, Dataset, random_split
-from torchvision.datasets import MNIST
+from torch.utils.data import DataLoader, Dataset, Subset
+
+from torchmeta.datasets import Omniglot
+from torchmeta.utils.data import BatchMetaDataLoader
+
 from torchvision.transforms import transforms
+from torchmeta.transforms import ClassSplitter, Categorical, Rotation
 
+from src.datamodules.components import CombinationRandomSampler
 
-class MNISTDataModule(LightningDataModule):
-    """Example of LightningDataModule for MNIST dataset.
-
-    A DataModule implements 5 key methods:
-
-        def prepare_data(self):
-            # things to do on 1 GPU/TPU (not on every GPU/TPU in DDP)
-            # download data, pre-process, split, save to disk, etc...
-        def setup(self, stage):
-            # things to do on every process in DDP
-            # load data, set variables, etc...
-        def train_dataloader(self):
-            # return train dataloader
-        def val_dataloader(self):
-            # return validation dataloader
-        def test_dataloader(self):
-            # return test dataloader
-        def teardown(self):
-            # called on every process in DDP
-            # clean up after fit or test
-
-    This allows you to share a full dataset without explaining how to download,
-    split, transform and process the data.
-
-    Read the docs:
-        https://pytorch-lightning.readthedocs.io/en/latest/data/datamodule.html
-    """
-
+class MetaDataModule(LightningDataModule):
     def __init__(
         self,
         data_dir: str = "data/",
-        train_val_test_split: Tuple[int, int, int] = (55_000, 5_000, 10_000),
+        kshot: int = 5,
+        nway: int = 5,
         batch_size: int = 64,
+        num_batches: DictConfig = DictConfig({"train": 10000, "val": 1000, "test": 1000}),
         num_workers: int = 2,
         pin_memory: bool = False,
     ):
@@ -51,9 +36,16 @@ class MNISTDataModule(LightningDataModule):
 
         # data transformations
         self.transforms = transforms.Compose(
-            [transforms.ToTensor(), 
+            [transforms.Resize(size=28),
+             transforms.ToTensor(), 
              transforms.Normalize((0.1307,), (0.3081,))]
         )
+
+        # target transformations
+        self.target_transform = Categorical()
+
+        # class augmentations
+        self.class_augmentations = [] #[Rotation(angle=a) for a in [90, 180, 270]]
 
         self.data_train: Optional[Dataset] = None
         self.data_val: Optional[Dataset] = None
@@ -61,57 +53,77 @@ class MNISTDataModule(LightningDataModule):
 
     @property
     def num_classes(self):
-        return 10
+        return self.hparams.nway
 
     def prepare_data(self):
-        """Download data if needed.
+        Omniglot(root=self.hparams.data_dir, num_classes_per_task=self.hparams.nway, meta_split="train", download=True)
+        Omniglot(root=self.hparams.data_dir, num_classes_per_task=self.hparams.nway, meta_split="val", download=True)
+        Omniglot(root=self.hparams.data_dir, num_classes_per_task=self.hparams.nway, meta_split="test", download=True)
 
-        Do not use it to assign state (self.x = y).
-        """
-        MNIST(self.hparams.data_dir, train=True, download=True)
-        MNIST(self.hparams.data_dir, train=False, download=True)
+    def setup_dataset(self, meta_split="train"):
+        dataset = Omniglot(
+            root=self.hparams.data_dir,
+            num_classes_per_task=self.hparams.nway,
+            transform=self.transforms,
+            target_transform=self.target_transform,
+            class_augmentations=self.class_augmentations,
+            meta_split=meta_split, 
+            download=False)
+        dataset.seed(42)
+        dataset = ClassSplitter(
+            dataset,
+            shuffle=True,
+            random_state_seed=42,
+            num_support_per_class=self.hparams.kshot,
+            num_query_per_class=self.hparams.kshot)
+        return dataset
 
     def setup(self, stage: Optional[str] = None):
-        """Load data. Set variables: `self.data_train`, `self.data_val`, `self.data_test`.
-
-        This method is called by lightning with both `trainer.fit()` and `trainer.test()`, so be
-        careful not to execute things like random split twice!
-        """
         # load and split datasets only if not loaded already
-        if not self.data_train and not self.data_val and not self.data_test:
-            trainset = MNIST(self.hparams.data_dir, train=True, transform=self.transforms)
-            testset = MNIST(self.hparams.data_dir, train=False, transform=self.transforms)
-            dataset = ConcatDataset(datasets=[trainset, testset])
-            self.data_train, self.data_val, self.data_test = random_split(
-                dataset=dataset,
-                lengths=self.hparams.train_val_test_split,
-                generator=torch.Generator().manual_seed(42),
-            )
+        if stage is None or stage == "fit":
+            self.data_train = self.setup_dataset("train")
+            # self.data_train = Subset(data_train, random.sample(
+            #     population=range(len(data_train)), 
+            #     k=self.hparams.num_batches.train))
+
+            self.data_val = self.setup_dataset("val")
+            # self.data_val = Subset(data_val, random.sample(
+            #     population=range(len(data_val)),
+            #     k=self.hparams.num_batches.val))
+        if stage == "test":
+            self.data_test = self.setup_dataset("test")
+            # self.data_test = Subset(data_test, random.sample(
+            #     population=range(len(data_test)),
+            #     k=self.hparams.num_batches.test))
 
     def train_dataloader(self):
-        return DataLoader(
+        return BatchMetaDataLoader(
             dataset=self.data_train,
             batch_size=self.hparams.batch_size,
             num_workers=self.hparams.num_workers,
             pin_memory=self.hparams.pin_memory,
-            shuffle=True,
+            sampler=CombinationRandomSampler(self.data_train, num_samples=self.hparams.num_batches.train),
+            shuffle=False,
         )
 
     def val_dataloader(self):
-        return DataLoader(
+        return BatchMetaDataLoader(
             dataset=self.data_val,
             batch_size=self.hparams.batch_size,
             num_workers=self.hparams.num_workers,
             pin_memory=self.hparams.pin_memory,
+            sampler=CombinationRandomSampler(self.data_val, num_samples=self.hparams.num_batches.val),
             shuffle=False,
         )
 
     def test_dataloader(self):
-        return DataLoader(
+        return BatchMetaDataLoader(
             dataset=self.data_test,
             batch_size=self.hparams.batch_size,
             num_workers=self.hparams.num_workers,
             pin_memory=self.hparams.pin_memory,
+            sampler=CombinationRandomSampler(
+                self.data_test, num_samples=self.hparams.num_batches.test),
             shuffle=False,
         )
 
